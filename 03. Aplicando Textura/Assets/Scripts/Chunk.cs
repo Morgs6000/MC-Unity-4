83f08ce0841019b5c5c0e0aee50084bb611fca0b
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2023-03-26 09:26:43 -0300

    Aplicando Textura

---
+++ 03. Aplicando Textura/Assets/Scripts/Chunk.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+    private Mesh voxelMesh;
+    
+    private List<Vector3> vertices = new List<Vector3>();    
+    private List<int> triangles = new List<int>();
+    private int vertexIndex;
+
+    private List<Vector2> uv = new List<Vector2>();
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
+    private void Awake() {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    
+    private void Start() {
+        ChunkGen();
+    }
+
+    private void Update() {
+        
+    }
+
+    public void ChunkGen() {
+        voxelMesh = new Mesh();
+        voxelMesh.name = "Chunk";
+
+        VoxelGen();
+        
+        MeshGen();
+    }
+
+    private void MeshGen() {
+        voxelMesh.vertices = vertices.ToArray();
+        voxelMesh.triangles = triangles.ToArray();
+        voxelMesh.uv = uv.ToArray();
+
+        voxelMesh.RecalculateBounds();
+        voxelMesh.RecalculateNormals();
+        voxelMesh.Optimize();
+
+        meshFilter.mesh = voxelMesh;
+    }
+
+    private void VoxelGen() {
+        for(int voxelSide = 0; voxelSide < 6; voxelSide++) {
+            for(int verts = 0; verts < 4; verts++) {
+                vertices.Add(VoxelData.vertices[voxelSide, verts]);
+
+                uv.Add(VoxelData.uv(new Vector2(1, 0))[verts]);
+            }
+
+            for(int tris = 0; tris < 6; tris++) {
+                triangles.Add(VoxelData.triangles[tris] + vertexIndex);
+            }
+
+            vertexIndex += 4;
+        }
+    }
+}
